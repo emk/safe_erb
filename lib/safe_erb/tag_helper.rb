758935239d9b4e66f60d65cbe21688dd8e081d8e
Eric Kidd <git@randomhacks.net>
2008-12-13 09:51:27 -0500

    Temporarily work around various Rails helpers
    These two Rails helpers cause Mephisto to fail massively under SafeERB.
    I'm not quite sure what's going wrong internally, because both of these
    helpers should be escaping their HTML.  I've read the source code, and
    I don't see where these helpers are picking up any kind of legitimate
    taint.
    
    Pending further investigation, I'm going to (perhaps unsafely) mark the
    return values of these two helpers as trusted.

--- lib/safe_erb/tag_helper.rb
+++ lib/safe_erb/tag_helper.rb
@@ -7,5 +7,23 @@ module ActionView
       
       alias_method_chain :escape_once, :untaint
     end
+
+    module DateHelper
+      # TODO - This is almost certainly too aggressive, and it will need to
+      # be fine-tuned.
+      def datetime_select_with_untaint(*args)
+        datetime_select_without_untaint(*args).untaint
+      end
+      alias_method_chain :datetime_select, :untaint
+    end
+
+    module ActiveRecordHelper
+      # TODO - This is almost certainly too aggressive, and it will need to
+      # be fine-tuned.
+      def error_messages_for_with_untaint(*args)
+        error_messages_for_without_untaint(*args).untaint
+      end
+      alias_method_chain :error_messages_for, :untaint
+    end
   end
 end
--- test/tag_helper_test.rb
+++ test/tag_helper_test.rb
@@ -2,6 +2,8 @@ require File.expand_path(File.dirname(__FILE__) + '/test_helper')
 
 class TagHelperTest < Test::Unit::TestCase
   include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::DateHelper
+  include ActionView::Helpers::ActiveRecordHelper
 
   def test_inclusion_in_taghelper
     assert self.respond_to?(:escape_once_with_untaint)
@@ -13,4 +15,16 @@ class TagHelperTest < Test::Unit::TestCase
     assert !escape_once(evil_str).tainted?
     assert escape_once_without_untaint(evil_str).tainted?
   end
+
+  Post = Struct.new(:published_at)
+  def post
+    @post ||= Post.new(Time.now.taint)
+  end
+
+  def test_datetime_select_should_untaint
+    assert !datetime_select(:post, :published_at).tainted?
+  end
+
+  # TODO - Add tests for error_messages_for helper.  This is a little
+  # tricky, because we'll need an ActiveRecord::Base instance.
 end
