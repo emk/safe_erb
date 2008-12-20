7ecced56d6a65df59a0a2643f7e93151c9ecfa7c
Eric Kidd <git@randomhacks.net>
2008-12-20 16:49:48 -0500

    Only apply safe_erb to ActionView::Renderable
    We were applying safe_erb to all ERB invocations that weren't on our
    whitelisted set of file extensions.  But this broke script/generate.
    So after a bit of tweaking, this patch limits safe_erb to all instances
    of ActionView::Renderable that aren't on our whitelist.
    
    I've done a fair bit of testing, using both automated tests and manual
    tests.  So far, safe_erb seems to be working.
    
    This code is fairly fragile, because the internals of ActionView are
    pretty weird, especially when view caching is turned on.  It's fairly
    hard to write a unit test which proves that safe_erb is actually
    working when config.action_controller.perform_caching is true.

--- lib/safe_erb/action_view_extensions.rb
+++ lib/safe_erb/action_view_extensions.rb
@@ -1,20 +1,18 @@
 module ActionView
-  module TemplateHandlers
-    class ERB < TemplateHandler
-      def compile_with_safe_erb(template)
-        # This helps make new-style ActionMailer text templates do the
-        # right thing automatically.  We will probably want to extend this
-        # to other kinds of templates eventually.
-        if template.filename.to_s =~ /\.text\.plain\.erb$/
-          ::ERB.without_checking_tainted do
-            compile_without_safe_erb template
-          end
-        else
-          compile_without_safe_erb template
+  Renderable.class_eval do
+    def compiled_source_with_safe_erb
+      # This helps make new-style ActionMailer text templates do the
+      # right thing automatically.  We will probably want to extend this
+      # to other kinds of templates eventually.
+      if filename.to_s =~ /\.text\.plain\.erb\z/
+        compiled_source_without_safe_erb
+      else
+        ::ERB.with_checking_tainted do
+          compiled_source_without_safe_erb
         end
       end
-
-      alias_method_chain :compile, :safe_erb
     end
+    
+    alias_method_chain :compiled_source, :safe_erb
   end
 end
--- safe_erb.gemspec
+++ safe_erb.gemspec
@@ -2,11 +2,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{safe_erb}
-  s.version = "0.1.1"
+  s.version = "0.1.2"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Shinya Kasatani", "Matthew Bass", "Eric Kidd"]
-  s.date = %q{2008-12-17}
+  s.date = %q{2008-12-20}
   s.email = %q{git@randomhacks.net}
   s.files = ["lib/safe_erb/action_view_extensions.rb", "lib/safe_erb/common.rb", "lib/safe_erb/erb_extensions.rb", "lib/safe_erb/rails_1.rb", "lib/safe_erb/rails_2.rb", "lib/safe_erb/sqlite3_fix.rb", "lib/safe_erb/tag_helper.rb", "lib/safe_erb.rb", "rails/init.rb", "test/safe_erb_test.rb", "test/tag_helper_test.rb", "test/test_helper.rb", "MIT-LICENSE", "Rakefile", "README"]
   s.homepage = %q{http://github.com/emk/safe_erb/tree/master}
